7bf8f4fe42620b5933be15dae21a088302a52b28
Kacper Rozdolski <k.rpzdolski@gmail.com>
2021-04-13 23:50:22 +0200

    Initial

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="marketing">
+    <main-hero></main-hero>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import MainHero from "./components/MainHero.vue";
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: { MainHero },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0 !important;
+}
+.marketing {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
 }
 </style>
---
+++ src/components/MainHero.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="hero"></section>
+</template>
+
+<script>
+export default {
+  name: "MainHero",
+};
+</script>
+
+<style lang="scss" scoped>
+.hero {
+  width: 100%;
+  height: 100vh;
+}
+</style>
